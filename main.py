49b917cfbd61d52746aadb9c7cc44937edbd2541
lummi <nothanks@github.com>
2026-08-02 23:06:41 +0300

    did the thing

--- main.py
+++ main.py
@@ -1,67 +1,51 @@
-import time
 import os
-import rich
-import getpass
-import subprocess
 import socket
-import ipaddress
+import getpass
+import rich
 from mac_vendor_lookup import MacLookup
-from scapy.all import IP, ARP, Ether, srp
-from scapy.all import conf
+from scapy.all import ARP, Ether, srp, conf
 from rich.console import Console
 
-conf.verb = 0
 console = Console()
+
 mac = MacLookup()
+try:
+    mac.update_vendors()
+except Exception:
+    pass
+
 wuser = getpass.getuser()
-os.system('cls')
+os.system('cls' if os.name == 'nt' else 'clear')
 
 s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-s.connect(("192.168.1.1", 80)) 
-local_ip = s.getsockname()[0]
+s.connect(("8.8.8.8", 80))
+my_ip = s.getsockname()[0]
 s.close()
 
-subnet = str(ipaddress.IPv4Network(f"{local_ip}/255.255.255.0", strict=False))
+conf.iface = next((i for i in conf.ifaces.values() if getattr(i, 'ip', None) == my_ip), conf.iface)
+base = ".".join(my_ip.split(".")[:-1]) + '.0/24'
 
 console.print("""
 [#2021b9]            _   _ _            [/]
 [#5a70f6]  ___  _   _| |_| (_)_ __   ___ [/]
 [#5192fa] / _ \\| | | | __| | | '_ \\ / _ \\ [/]
 [#a3b4cc]| (_) | |_| | |_| | | | | |  __/[/]
-[#c1d4cd] \\___/ \\__,_|\\__|_|_|_| |_|\\___/[/]
+[#c1d4cd] \\___/ \\__,_|\__|_|_|_| |_|\\___/[/]
 """)
 while True:
-    print('')
     command = console.input(f'[[#79b2fc]{wuser}[/]|user] - ')
 
-
     if command in ('help', 'helpita'):
-        print("""
-scan - scans all the devices it can find on ur ip 
-""")
+        print("\nscan - scans all the devices it can find on ur ip\n")
 
     elif command in ('scan', 'scanita'):
-        s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-        s.connect(("8.8.8.8", 80))
-        local_ip = s.getsockname()[0]
-        s.close()
-
-        subnet = local_ip.rsplit('.', 1)[0] + '.0/24'
-
-        # Auto-detect and bind Scapy to the exact physical adapter holding local_ip
-        try:
-            target_iface = [iface for iface in conf.ifaces.values() if iface.ip == local_ip][0]
-            conf.iface = target_iface
-        except IndexError:
-            target_iface = conf.iface
-
-        packet = Ether(dst="ff:ff:ff:ff:ff:ff") / ARP(pdst=subnet)
+        console.print(f"[dim]Scanning target: {base} on interface: {conf.iface.name}[/dim]")
         
-        # Explicitly pass iface to srp
-        answered, unanswered = srp(packet, timeout=3, verbose=False, iface=conf.iface)
+        packet = Ether(dst="ff:ff:ff:ff:ff:ff") / ARP(pdst=base)
+        answered, _ = srp(packet, timeout=3, verbose=False, iface=conf.iface)
 
         if not answered:
-            console.print("[red]No devices responded[/]")
+            console.print("[bold red]No devices found.[/bold red]")
         else:
             for _, received in answered:
                 ip = received.psrc
@@ -69,7 +53,7 @@ scan - scans all the devices it can find on ur ip
 
                 try:
                     name = socket.gethostbyaddr(ip)[0]
-                except (socket.herror, socket.gaierror):
+                except socket.herror:
                     name = None
 
                 if not name:
@@ -77,16 +61,12 @@ scan - scans all the devices it can find on ur ip
                         vendor = mac.lookup(mac_addr)
                         name = f"{vendor} device"
                     except Exception:
-                        name = "unknown device"
+                        name = "Unknown device"
 
-                print(f"{ip}: {name} ({mac_addr})")
+                console.print(f"[bold green]{ip}[/bold green]: {name} [dim]({mac_addr})[/dim]")
 
     elif command in ('exit', 'exita', 'quit', 'quitita'):
-         break
-
-
-    elif command in ("placeholder"):
-         pass
+        break
 
     else:
-         console.print(f"[red]{command} isnt recognized as a command[/]")
+        console.print(f"[red]{command} isn't recognized as a command[/red]")
